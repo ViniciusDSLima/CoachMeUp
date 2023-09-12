adedc75e1baab3f6d78449cd43c82cf60d3bc304
Vinicius <vinicius.devjvm@gmail.com>
2023-09-11 22:20:51 -0300

    refatorando a classe Customer e a classe de requisicao do cliente para cadastro; criado o construtor para a criacao do Customer.

--- src/main/java/com/example/customerservice/domain/models/Customer.java
+++ src/main/java/com/example/customerservice/domain/models/Customer.java
@@ -2,6 +2,7 @@ package com.example.customerservice.domain.models;
 
 import com.example.customerservice.domain.address.Address;
 import com.example.customerservice.enums.CustomerRole;
+import com.example.customerservice.enums.Flat;
 import com.example.customerservice.request.CustomerRegisterRequest;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
@@ -29,12 +30,20 @@ public class Customer {
     private Address address;
     @Enumerated(EnumType.STRING)
     private CustomerRole role;
+    @Enumerated(EnumType.STRING)
+    private Flat flat;
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime register;
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime update;
 
     public Customer(CustomerRegisterRequest data) {
-        this.name = data.getName()
+        this.name = data.getName();
+        this.email = data.getEmail();
+        this.password = data.getPassword();
+        this.NIF = data.getNIF();
+        this.phone = data.getPhone();
+        this.address = data.getAddress();
+        this.role = getRole();
     }
 }
--- src/main/java/com/example/customerservice/request/CustomerRegisterRequest.java
+++ src/main/java/com/example/customerservice/request/CustomerRegisterRequest.java
@@ -1,6 +1,11 @@
 package com.example.customerservice.request;
 
+import com.example.customerservice.domain.address.Address;
 import com.example.customerservice.enums.CustomerRole;
+import com.example.customerservice.enums.Flat;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerRegisterRequest {
+    private String name;
     private String email;
     private String password;
+    private long NIF;
+    private long phone;
+    private Address address;
+    private Flat flat;
     private CustomerRole role;
+
 }
