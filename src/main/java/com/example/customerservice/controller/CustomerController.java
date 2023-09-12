ff6e96df2f100a90e03dc0e8894c00d27d239155
Vinicius <vinicius.devjvm@gmail.com>
2023-09-12 09:21:02 -0300

    Criado o metodo findById na classe CustomerController

--- src/main/java/com/example/customerservice/controller/CustomerController.java
+++ src/main/java/com/example/customerservice/controller/CustomerController.java
@@ -7,10 +7,7 @@ import lombok.AllArgsConstructor;
 import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -21,5 +18,10 @@ public class CustomerController {
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid CustomerRegisterRequest request){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(request));
+    }
+
+    @GetMapping("/findBy/{id}")
+    public ResponseEntity findById(@PathVariable("id") String id){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findById(id));
     }
 }
--- src/main/java/com/example/customerservice/exceptions/errors/ObjectNotFoundException.java
+++ src/main/java/com/example/customerservice/exceptions/errors/ObjectNotFoundException.java
@@ -1,11 +1,5 @@
 package com.example.customerservice.exceptions.errors;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.aop.ThrowsAdvice;
-
-@AllArgsConstructor
-@NoArgsConstructor
 public class ObjectNotFoundException extends RuntimeException{
 
     public ObjectNotFoundException(String message, Throwable cause){
