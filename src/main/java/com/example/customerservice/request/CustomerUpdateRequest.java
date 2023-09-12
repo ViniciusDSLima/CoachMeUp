0abfc8e9af8614c5fbddd168f36bb7c8f8c8d6bf
Vinicius <vinicius.devjvm@gmail.com>
2023-09-12 12:12:06 -0300

    Adicionada a interface ValidationsName para validar o campo de nomes.

---
+++ src/main/java/com/example/customerservice/request/CustomerUpdateRequest.java
@@ -0,0 +1,31 @@
+package com.example.customerservice.request;
+
+import com.example.customerservice.domain.address.Address;
+import com.example.customerservice.enums.CustomerRole;
+import com.example.customerservice.enums.Flat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerUpdateRequest {
+    private String name;
+    @Email
+    private String email;
+    @Pattern(regexp = "^(?:(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))(?!.*(.)\\1{2,})[A-Za-z0-9!~<>,;:_=?*+#.”&§%°()\\|\\[\\]\\-\\$\\^\\@\\/]{8,32}$",
+            message = "A senha exige de 4 a 32 caracteres, Letras maiusculas e minusculas, numeros e caracteres especiais.")
+    private String password;
+    private long NIF;
+    @Pattern(regexp = "\\(?\\+[0-9]{1,3}\\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})?"
+            , message = "enter a valid cell phone number")
+    private String phone;
+    private Address address;
+    private Flat flat;
+    private CustomerRole role;
+}
--- src/main/java/com/example/customerservice/service/CustomerService.java
+++ src/main/java/com/example/customerservice/service/CustomerService.java
@@ -2,11 +2,11 @@ package com.example.customerservice.service;
 
 import com.example.customerservice.DTO.CustomerDTO;
 import com.example.customerservice.domain.models.Customer;
-import com.example.customerservice.exceptions.errors.LengthException;
 import com.example.customerservice.exceptions.errors.ObjectNotFoundException;
 import com.example.customerservice.mapper.CustomerMapper;
 import com.example.customerservice.repository.CustomerRepository;
 import com.example.customerservice.request.CustomerRegisterRequest;
+import com.example.customerservice.validations.ValidationsName;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,9 +20,7 @@ public class CustomerService {
     @Transactional
     public CustomerDTO save(CustomerRegisterRequest customerRegisterRequest){
 
-        if(customerRegisterRequest.getName().length() < 3) throw new LengthException("The minimum length for the name is 3 characters");
-
-        if(customerRegisterRequest.getName().length() > 255) throw new LengthException("The maximum length for the name is 255 characters");
+        ValidationsName.valide(customerRegisterRequest);
 
         Customer customer = customerRepository.save(CustomerMapper.INSTANCE.toCustomer(customerRegisterRequest));
 
@@ -33,4 +31,6 @@ public class CustomerService {
         return CustomerMapper.INSTANCE.toCustomerDto(customerRepository.findById(id).orElseThrow(
                 () -> new ObjectNotFoundException("Customer Not Found")));
     }
+
+
 }
