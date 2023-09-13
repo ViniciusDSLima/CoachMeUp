493d129387e41abde2edb119d8e3241a4048d7af
Vinicius <vinicius.devjvm@gmail.com>
2023-09-13 11:11:16 -0300

    Adicionado os campos created_at e o updated_at ao customerDTO para retornar ao usuario quando ele criou o usuario e quando ele o atualizou.

--- src/main/java/com/example/customerservice/DTO/CustomerDTO.java
+++ src/main/java/com/example/customerservice/DTO/CustomerDTO.java
@@ -3,9 +3,12 @@ package com.example.customerservice.DTO;
 import com.example.customerservice.domain.address.Address;
 import com.example.customerservice.domain.models.Customer;
 import com.example.customerservice.enums.Flat;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -17,5 +20,8 @@ public class CustomerDTO {
     private String phone;
     private Address address;
     private Flat flat;
-
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime created_at = LocalDateTime.now();
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime updated_at = LocalDateTime.now();
 }
