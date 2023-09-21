cd1f43dc5e33feec2c6fb1674cd53fc49ecaaff4
Vinicius <vinicius.devjvm@gmail.com>
2023-09-21 14:58:54 -0300

    Criada a classe de validacao do Endereco.

---
+++ src/main/java/com/example/customerservice/validations/validacoes/AdressValidation.java
@@ -0,0 +1,11 @@
+package com.example.customerservice.validations.validacoes;
+
+import com.example.customerservice.request.CustomerRegisterRequest;
+import com.example.customerservice.validations.Validation;
+
+public class AdressValidation implements Validation {
+    @Override
+    public void validate(CustomerRegisterRequest request) {
+
+    }
+}
