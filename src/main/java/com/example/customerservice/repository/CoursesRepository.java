404663540c56ac1babda44f7e5c391944857eb1d
Vinicius <vinicius.devjvm@gmail.com>
2023-09-22 16:33:17 -0300

    criado o metodo findByCategory na CoursesRepository

--- src/main/java/com/example/customerservice/repository/CoursesRepository.java
+++ src/main/java/com/example/customerservice/repository/CoursesRepository.java
@@ -1,12 +1,16 @@
 package com.example.customerservice.repository;
 
+import com.example.customerservice.domain.models.Category;
 import com.example.customerservice.domain.models.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CoursesRepository extends JpaRepository<Courses, String> {
     Optional<Courses> findByName(String name);
+
+    Optional<List<Courses>> findByCategory(Category category);
 }
