62eb3fab3226bf258da412db39ad94f0cf80e38f
Vinicius <vinicius.devjvm@gmail.com>
2023-09-22 11:57:14 -0300

    refatorado o metodo findByName para retornar uma excecao quando ele nao for encontrado.

--- src/main/java/com/example/customerservice/service/CoursesService.java
+++ src/main/java/com/example/customerservice/service/CoursesService.java
@@ -2,6 +2,7 @@ package com.example.customerservice.service;
 
 import com.example.customerservice.DTO.CoursesDTO;
 import com.example.customerservice.domain.models.Courses;
+import com.example.customerservice.exceptions.errors.UndefinedName;
 import com.example.customerservice.mapper.CoursesMapper;
 import com.example.customerservice.repository.CoursesRepository;
 import jakarta.transaction.Transactional;
@@ -15,7 +16,8 @@ public class CoursesService {
     private CoursesRepository coursesRepository;
 
     public CoursesDTO findByName(String name){
-        Courses course = coursesRepository.findByName(name);
+        Courses course = coursesRepository.findByName(name)
+                .orElseThrow(() -> new UndefinedName("Curso nao encontrado."));
 
         return CoursesMapper.INSTANCE.toCoursesDTO(course);
     }
