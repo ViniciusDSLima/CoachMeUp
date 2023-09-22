5409e6a47cea60209e31832b1a9e72dcf2fc7609
Vinicius <vinicius.devjvm@gmail.com>
2023-09-22 11:51:37 -0300

    Criado os dois metodos na service(save e findByName).

---
+++ src/main/java/com/example/customerservice/service/CoursesService.java
@@ -0,0 +1,30 @@
+package com.example.customerservice.service;
+
+import com.example.customerservice.DTO.CoursesDTO;
+import com.example.customerservice.domain.models.Courses;
+import com.example.customerservice.mapper.CoursesMapper;
+import com.example.customerservice.repository.CoursesRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CoursesService {
+
+    private CoursesRepository coursesRepository;
+
+    public CoursesDTO findByName(String name){
+        Courses course = coursesRepository.findByName(name);
+
+        return CoursesMapper.INSTANCE.toCoursesDTO(course);
+    }
+
+    @Transactional
+    public CoursesDTO save(Courses courses){
+        Courses course = coursesRepository.save(courses);
+
+        return CoursesMapper.INSTANCE.toCoursesDTO(course);
+    }
+
+}
