d096a960da989f6b904676042bf4ddd2fc93a4f4
Vinicius <vinicius.devjvm@gmail.com>
2023-09-22 16:34:15 -0300

    Criado o metodo findById que ira servir para buscar os cursos pela categoria.

--- src/main/java/com/example/customerservice/service/CoursesService.java
+++ src/main/java/com/example/customerservice/service/CoursesService.java
@@ -1,13 +1,18 @@
 package com.example.customerservice.service;
 
 import com.example.customerservice.DTO.CoursesDTO;
+import com.example.customerservice.domain.models.Category;
 import com.example.customerservice.domain.models.Courses;
+import com.example.customerservice.exceptions.errors.ObjectNotFoundException;
 import com.example.customerservice.exceptions.errors.UndefinedName;
 import com.example.customerservice.mapper.CoursesMapper;
 import com.example.customerservice.repository.CoursesRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -19,14 +24,21 @@ public class CoursesService {
         Courses course = coursesRepository.findByName(name)
                 .orElseThrow(() -> new UndefinedName("Curso nao encontrado."));
 
-        return CoursesMapper.INSTANCE.toCoursesDTO(course);
+        return CoursesMapper.INSTANCE.toCourseDTO(course);
+    }
+
+    public List<CoursesDTO> findByCategory(@Valid Category category){
+        List<Courses> courses = coursesRepository.findByCategory(category)
+                .orElseThrow(() -> new ObjectNotFoundException("Nao ha cusos cadastrados com essa categoria."));
+
+        return CoursesMapper.INSTANCE.toCoursesDTO(courses);
     }
 
     @Transactional
     public CoursesDTO save(Courses courses){
         Courses course = coursesRepository.save(courses);
 
-        return CoursesMapper.INSTANCE.toCoursesDTO(course);
+        return CoursesMapper.INSTANCE.toCourseDTO(course);
     }
 
 }
