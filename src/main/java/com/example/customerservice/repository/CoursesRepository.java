f5d00d74d686dfeffbab3470df0dab224d41a57a
Vinicius <vinicius.devjvm@gmail.com>
2023-09-22 11:56:28 -0300

    refatorada para retornar um optional na busca pelo nome do curso.

--- src/main/java/com/example/customerservice/repository/CoursesRepository.java
+++ src/main/java/com/example/customerservice/repository/CoursesRepository.java
@@ -4,7 +4,9 @@ import com.example.customerservice.domain.models.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CoursesRepository extends JpaRepository<Courses, String> {
-    Courses findByName(String name);
+    Optional<Courses> findByName(String name);
 }
