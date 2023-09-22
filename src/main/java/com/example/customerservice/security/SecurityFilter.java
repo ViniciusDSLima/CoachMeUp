6e45eaf0699c9ada7d9383e812aa0b13c2a4e183
Vinicius <vinicius.devjvm@gmail.com>
2023-09-22 19:44:19 -0300

    Criado o filtro de rotas da aplicacao

---
+++ src/main/java/com/example/customerservice/security/SecurityFilter.java
@@ -0,0 +1,46 @@
+package com.example.customerservice.security;
+
+import com.example.customerservice.service.UsuarioService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+    TokenService tokenService;
+
+    UsuarioService usuarioService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = recoverToken(request);
+        if(token != null){
+            var email = tokenService.verifyToken(token);
+            UserDetails usuario = usuarioService.loadUserByUsername(String.valueOf(email));
+
+            var authetication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authetication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null){
+            return null;
+        }
+        return authHeader.replace("Bearer", "");
+    }
+
+
+}
