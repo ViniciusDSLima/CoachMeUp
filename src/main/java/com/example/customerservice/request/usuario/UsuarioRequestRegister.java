3b23dfa69de647ce9d533e6f4df11db6382f6344
Vinicius <vinicius.devjvm@gmail.com>
2023-09-23 15:25:23 -0300

    Criada a classe de requisicao do usuario para se cadastrar.

---
+++ src/main/java/com/example/customerservice/request/usuario/UsuarioRequestRegister.java
@@ -0,0 +1,27 @@
+package com.example.customerservice.request.usuario;
+
+import com.example.customerservice.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioRequestRegister {
+    @NotBlank
+    private String name;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Pattern(regexp = "^(?:(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))(?!.*(.)\\1{2,})[A-Za-z0-9!~<>,;:_=?*+#.”&§%°()\\|\\[\\]\\-\\$\\^\\@\\/]{8,32}$",
+            message = "A senha exige de 4 a 32 caracteres, Letras maiusculas e minusculas, numeros e caracteres especiais.")
+    private String password;
+    @NotNull
+    private UserRole role;
+}
