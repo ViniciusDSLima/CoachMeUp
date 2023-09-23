51bff59048c9545b5817b79b173b6cf71d7b8c79
Vinicius <vinicius.devjvm@gmail.com>
2023-09-23 15:27:17 -0300

    Criado o mapeamento da entidade Usuario para retornar as informacoes para o cliente

---
+++ src/main/java/com/example/customerservice/mapper/UsuarioMapper.java
@@ -0,0 +1,16 @@
+package com.example.customerservice.mapper;
+
+import com.example.customerservice.DTO.UsuarioDTO;
+import com.example.customerservice.domain.models.Usuario;
+import com.example.customerservice.request.usuario.UsuarioRequestRegister;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UsuarioMapper {
+
+    UsuarioMapper INSTANCE = Mappers.getMapper(UsuarioMapper.class);
+    Usuario toUsuario(UsuarioRequestRegister request);
+    UsuarioDTO toUsuarioDTO(Usuario usuario);
+
+}
