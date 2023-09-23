a24f4639bbcbd8e9bcaeec46db637cdd648c692f
Vinicius <vinicius.devjvm@gmail.com>
2023-09-23 15:28:23 -0300

    Criada a classe de reposta para o Usuario ao se cadastrar ou logar.

---
+++ src/main/java/com/example/customerservice/DTO/UsuarioDTO.java
@@ -0,0 +1,13 @@
+package com.example.customerservice.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+    private String name;
+    private String email;
+}
