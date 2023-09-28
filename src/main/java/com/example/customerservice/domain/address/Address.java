3f48880b9a95a5ced1a9f8e88fa324f32b552890
Vinicius <vinicius.devjvm@gmail.com>
2023-09-27 21:12:05 -0300

    Refatorada a classe Address para a utilizacao da API do Ctt

--- src/main/java/com/example/customerservice/domain/address/Address.java
+++ src/main/java/com/example/customerservice/domain/address/Address.java
@@ -11,34 +11,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
-    private String street;
-    private int postalCode;
-    private String houseNumber;
-    private String parish;
-    private String municipalities;
-    private String district;
-    private String complement;
+    private String morada;
+    private String codigoPostal;
+    private String porta;
+    private String freguesia;
+    private String concelho;
+    private String distrito;
+    private String localidade;
 
-    public Address(DataAddress address) {
-        this.street = address.getStreet();
-        this.postalCode = address.getPostalCode();
-        this.houseNumber = address.getHouseNumber();
-        this.parish = address.getParish();
-        this.municipalities = address.getMunicipalities();
-        this.district = address.getDistrict();
-        this.complement = address.getComplement();
-    }
-
-// REFATORACAO PARA A API "IBGE"
 
     public void updateInfo(DataAddress address){
-        if(address.getStreet() != null) this.street = address.getStreet();
-        if(address.getPostalCode() != 0) this.postalCode = address.getPostalCode();
-        if(address.getHouseNumber() != null) this.houseNumber = address.getHouseNumber();
-        if(address.getParish() != null) this.parish = address.getParish();
-        if(address.getMunicipalities() != null) this.municipalities = address.getMunicipalities();
-        if(address.getDistrict() != null) this.district = address.getDistrict();
-        if(address.getComplement() != null) this.complement = address.getComplement();
+        if(address.getMorada() != null) this.morada = address.getMorada();
+        if(address.getCodigoPostal() != null) this.codigoPostal = address.getCodigoPostal();
+        if(address.getPorta() != null) this.porta = address.getPorta();
+        if(address.getFreguesia() != null) this.freguesia = address.getFreguesia();
+        if(address.getConcelho() != null) this.concelho = address.getConcelho();
+        if(address.getDistrito() != null) this.distrito = address.getDistrito();
+        if(address.getLocalidade() != null) this.localidade = address.getLocalidade();
     }
 
 }
