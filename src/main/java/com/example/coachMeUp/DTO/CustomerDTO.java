1083c1d62b4c687e4075c667a0531e085beafbd9
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-09-29 17:51:12 -0300

    Refatorado o customerDTO para o numero e para o Nif

--- src/main/java/com/example/coachMeUp/DTO/CustomerDTO.java
+++ src/main/java/com/example/coachMeUp/DTO/CustomerDTO.java
@@ -1,10 +1,13 @@
 package com.example.coachMeUp.DTO;
 
 import com.example.coachMeUp.domain.address.Address;
+import com.example.coachMeUp.domain.entities.Phone;
 import com.example.coachMeUp.enums.Flat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -12,8 +15,8 @@ import lombok.NoArgsConstructor;
 public class CustomerDTO {
     private String name;
     private String email;
-    private long NIF;
-    private String phone;
+    private String NIF;
+    private List<Phone> phone;
     private Address address;
     private Flat flat;
 }
