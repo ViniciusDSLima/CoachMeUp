f476cf12672debb2e187149a7e2953e144d19a33
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-09-29 17:51:58 -0300

    Refatorado o A customerRegisterUpdate para a requisicao do usuario de atualizacao.

--- src/main/java/com/example/coachMeUp/request/customer/CustomerUpdateRequest.java
+++ src/main/java/com/example/coachMeUp/request/customer/CustomerUpdateRequest.java
@@ -1,10 +1,13 @@
 package com.example.coachMeUp.request.customer;
 
+import com.example.coachMeUp.domain.entities.Phone;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -16,8 +19,6 @@ public class CustomerUpdateRequest {
     @Pattern(regexp = "^(?:(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*\\d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))(?!.*(.)\\1{2,})[A-Za-z0-9!~<>,;:_=?*+#.”&§%°()\\|\\[\\]\\-\\$\\^\\@\\/]{8,32}$",
             message = "A senha exige de 4 a 32 caracteres, Letras maiusculas e minusculas, numeros e caracteres especiais.")
     private String password;
-    @Pattern(regexp = "\\(?\\+[0-9]{1,3}\\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})?"
-            , message = "enter a valid cell phone number")
-    private String phone;
+    private List<Phone> phone;
     private DataAddress address;
 }
