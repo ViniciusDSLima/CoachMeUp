9b5a33782abe68f48ec14a447f415779ec542dd7
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-09-29 17:54:20 -0300

    Criada a classe Phone que tem relacionamento com o Cliente.

---
+++ src/main/java/com/example/coachMeUp/domain/entities/Phone.java
@@ -0,0 +1,27 @@
+package com.example.coachMeUp.domain.entities;
+
+import com.example.coachMeUp.enums.TypeNumber;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Pattern(regexp = "\\(?\\+[0-9]{1,3}\\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})?"
+            , message = "enter a valid cell phone number")
+    private String number;
+    @Enumerated(EnumType.STRING)
+    private TypeNumber typeNumber;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_customer")
+    private Customer customer;
+}
