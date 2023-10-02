b60b2585f9cab0c934b40b7ac83ea8c5f2d371c4
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-02 09:16:02 -0300

    Criado o metodo para a criacao de cursos.

--- src/main/java/com/example/coachMeUp/mapper/CoursesMapper.java
+++ src/main/java/com/example/coachMeUp/mapper/CoursesMapper.java
@@ -2,6 +2,7 @@ package com.example.coachMeUp.mapper;
 
 import com.example.coachMeUp.DTO.CoursesDTO;
 import com.example.coachMeUp.domain.entities.Courses;
+import com.example.coachMeUp.request.CoursesRegisterRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -13,7 +14,9 @@ public interface CoursesMapper {
     CoursesMapper INSTANCE = Mappers.getMapper(CoursesMapper.class);
 
 
-    CoursesDTO toCourseDTO(Courses course);
+    CoursesDTO toCourseDTO(Courses courses);
 
     List<CoursesDTO> toCoursesDTO(List<Courses> course);
+
+    Courses toCustomer(CoursesRegisterRequest request);
 }
