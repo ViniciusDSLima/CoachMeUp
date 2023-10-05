7d16c44fbf5059b24b34e6e9edc787479d5e355e
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-05 15:42:14 -0300

    Criada as permissoes gerais.

---
+++ src/main/java/com/example/coachMeUp/permissions/GeneralPermissions.java
@@ -0,0 +1,12 @@
+package com.example.coachMeUp.permissions;
+
+public interface GeneralPermissions {
+
+    final String[] PERMISSOES_GERAIS = {
+            "/login",
+            "/api/v1/usuario/register",
+            "/api/v1/courses/{name}",
+            "/api/v1/courses/categories/{category}",
+
+    };
+}
