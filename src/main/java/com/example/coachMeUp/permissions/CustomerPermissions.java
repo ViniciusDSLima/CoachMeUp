6f377f2a1fb5adc51d6ba60491dec9ac704d8b50
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-05 15:39:06 -0300

    Criada as rotas de permissoes de clientes.

---
+++ src/main/java/com/example/coachMeUp/permissions/CustomerPermissions.java
@@ -0,0 +1,12 @@
+package com.example.coachMeUp.permissions;
+
+public interface CustomerPermissions{
+
+
+    final String[] PERMISSOES_ASSINANTES = {
+            "/api/v1/customers/update/{id}",
+            "/api/v1/customers/update/{id}",
+            "/api/v1/courses/category/{category}",
+            "/api/v1/courses/{name}"
+    };
+}
