3a6510fcb43ae15f158e60979596c7cb9bf2078f
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-18 14:52:28 -0300

    Renomeado para AuthServiceImpl

--- src/main/java/com/example/coachMeUp/service/AuthServiceImpl.java
+++ src/main/java/com/example/coachMeUp/service/AuthServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UsuarioServiceImpl implements UserDetailsService {
+public class AuthServiceImpl implements UserDetailsService {
 
     UsuarioRepository usuarioRepository;
     @Override
