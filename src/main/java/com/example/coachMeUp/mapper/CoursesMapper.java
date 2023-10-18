6093afaf63412528feb611e0972c781f1e145069
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-18 14:57:37 -0300

    Adicionada a importacao da classe Course

--- src/main/java/com/example/coachMeUp/mapper/CoursesMapper.java
+++ src/main/java/com/example/coachMeUp/mapper/CoursesMapper.java
@@ -1,8 +1,8 @@
 package com.example.coachMeUp.mapper;
 
 import com.example.coachMeUp.DTO.CoursesDTO;
-import com.example.coachMeUp.domain.entities.Courses;
-import com.example.coachMeUp.request.CoursesRegisterRequest;
+import com.example.coachMeUp.domain.entities.Course;
+import com.example.coachMeUp.request.course.CoursesRegisterRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -14,9 +14,9 @@ public interface CoursesMapper {
     CoursesMapper INSTANCE = Mappers.getMapper(CoursesMapper.class);
 
 
-    CoursesDTO toCourseDTO(Courses courses);
+    CoursesDTO toCourseDTO(Course course);
 
-    List<CoursesDTO> toCoursesDTO(List<Courses> course);
+    List<CoursesDTO> toCoursesDTO(List<Course> course);
 
-    Courses toCustomer(CoursesRegisterRequest request);
+    Course toCustomer(CoursesRegisterRequest request);
 }
