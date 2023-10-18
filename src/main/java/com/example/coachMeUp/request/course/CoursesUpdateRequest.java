5a019738d9666f625d7db2f7557fe223defc085c
Vinicius Lima <vinicius.devjvm@gmail.com>
2023-10-18 14:59:37 -0300

    Retirada a importacao da classe Course e retirado o paramentro price da requisicao de atualizacao do curso.

--- src/main/java/com/example/coachMeUp/request/course/CoursesUpdateRequest.java
+++ src/main/java/com/example/coachMeUp/request/course/CoursesUpdateRequest.java
@@ -5,14 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CoursesUpdateRequest {
 
     private String name;
-    private BigDecimal price;
     private Category category;
 }
